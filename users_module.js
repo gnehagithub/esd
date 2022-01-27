063028ea1e757d546d2d285c2a7324470ced7980
gnehagithub <neha30gupta10@gmail.com>
2022-01-27 19:45:01 +0530

    changes

--- users_module.js
+++ users_module.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 //mongodb://127.0.0.1:27017/dbname
 //const conn_str = "mongodb://localhost:27017/tcet";
-const conn_str = "mongodb+srv://ESD:esd123@cluster0.jm1xt.mongodb.net/esd33?retryWrites=true&w=majority";
+const conn_str = "mongodb+srv://ESD:esd123@cluster0.jm1xt.mongodb.net/Esd33?retryWrites=true&w=majority";
 mongoose.connect(conn_str, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log("Connected successfully..."))
 .catch( (error) => console.log(error) );
